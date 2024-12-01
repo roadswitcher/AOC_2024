353c4fb94f8499926cea8313e71ecd3c36fdac62
Bill Blum <bill.blum@gmail.com>
2024-12-01 15:11:26 -0500

    Minor tweaks.

--- Day_1/day1.cpp
+++ Day_1/day1.cpp
@@ -12,15 +12,15 @@ using location_vector = std::vector<int>;
 std::pair<location_vector, location_vector>
 read_location_lists(const std::string &filename) {
   location_vector a, b;
-  std::string line;
-  std::string tmp_a, tmp_b;
+  std::string line{};
+  int tmp_a, tmp_b;
 
   std::ifstream fs(filename);
   while (std::getline(fs, line)) {
     std::istringstream values(line);
     values >> tmp_a >> tmp_b;
-    a.push_back(std::stoi(tmp_a));
-    b.push_back(std::stoi(tmp_b));
+    a.push_back(tmp_a);
+    b.push_back(tmp_b);
   }
   std::sort(a.begin(), a.end());
   std::sort(b.begin(), b.end());
@@ -37,7 +37,7 @@ std::vector<int> difference_list(location_vector &a, location_vector &b) {
 
 auto similarity_metric(location_vector &a, location_vector &b) {
   std::unordered_map<int, int> freq_map_b;
-  int result = 0;
+  int result{};
 
   for (auto &elem : b) {
     freq_map_b[elem]++;
