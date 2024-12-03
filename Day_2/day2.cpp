aa7aca7d2022387ce4828d0eba51b9692d691ffb
Bill Blum <bill.blum@gmail.com>
2024-12-02 19:20:08 -0500

    Saving state.

--- Day_2/day2.cpp
+++ Day_2/day2.cpp
@@ -1,27 +1,20 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <iterator>
 #include <numeric>
 #include <sstream>
 #include <string>
 #include <unordered_map>
 #include <vector>
-#include <iterator>
 
 using input_report = std::vector<int>;
 
 bool report_is_safe(input_report report) {
-
-  // std::copy(report.begin(), report.end(), std::ostream_iterator<int>(std::cout, " "));
-  // std::cout << "\n";
-
   // Heard about this on the ADSP podcast -- finally, an excuse to use it?
   std::adjacent_difference(report.begin(), report.end(), report.begin());
   report.erase(report.begin()); // drop the first element
-  
-  // std::copy(report.begin(), report.end(), std::ostream_iterator<int>(std::cout, " "));
-  // std::cout << "\n";
-  
+
   // If any difference is zero, fail
   if (std::find(report.begin(), report.end(), 0) != report.end())
     return false;
@@ -42,9 +35,24 @@ bool report_is_safe(input_report report) {
   return true;
 }
 
+bool problem_dampener(input_report report) {
+  // If report can be rendered safe removing a single element, it's safe.
+  for (int idx = 0; idx < report.size(); idx++) {
+    input_report tmp_report;
+    for (int j = 0; j < report.size(); j++) {
+      if (j != idx)
+        tmp_report.push_back(report[j]);
+    }
+    if (report_is_safe(tmp_report)) {
+      return true;
+    }
+  }
+  // Fell thru, return false
+  return false;
+}
+
 void process_reports(std::string filename) {
   std::ifstream fs(filename);
-  std::cout << "Opened " << filename <<"\n";
 
   std::string line;
   int num_safe = 0;
@@ -53,25 +61,46 @@ void process_reports(std::string filename) {
     std::istringstream values(line);
     int num;
     input_report test_report;
-    while(values>>num) {
+    while (values >> num) {
       test_report.push_back(num);
     }
 
-    if (report_is_safe(test_report))
+    if (report_is_safe(test_report)) {
       num_safe += 1;
+    }
   }
-
   std::cout << "Part 1: Number of safe reports: " << num_safe << "\n";
+}
+
+void process_reports_part_II(std::string filename) {
+  std::ifstream fs(filename);
+
+  std::string line;
+  int num_safe = 0;
+
+  while (std::getline(fs, line)) {
+    std::istringstream values(line);
+    int num;
+    input_report test_report;
+    while (values >> num) {
+      test_report.push_back(num);
+    }
+
+    if (report_is_safe(test_report)) {
+      num_safe += 1;
+    } else if (problem_dampener(test_report)) {
+      num_safe += 1;
+    }
+  }
+  std::cout << "Part 2: Number of safe reports: " << num_safe << "\n";
 }
 
 int main() {
   std::string fname = "input.txt";
-  process_reports(fname);
-  // location_vector list_a, list_b;
-  // std::tie(list_a, list_b) = read_location_lists("input.txt");
+  // std::string fname = "test_input.txt";
 
-  // std::cout << part1(list_a, list_b) << "\n";
-  // std::cout << part2(list_a, list_b) << "\n";
+  process_reports(fname);
+  process_reports_part_II(fname);
 
   return 0;
 }
