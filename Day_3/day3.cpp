69154c2cbad2e614d700e5bc7a5139317711f772
Bill Blum <bill.blum@gmail.com>
2024-12-03 17:41:37 -0500

    Saving state, Day 3 Pt 1 complete.

--- Day_3/day3.cpp
+++ Day_3/day3.cpp
@@ -1,20 +1,39 @@
+#include <cstdint>
+#include <fstream>
 #include <iostream>
 #include <regex>
-#include <fstream>
 #include <string>
 
-// part 1:
-// obvious approach:
-// - iterate over the file
-// - use regex to find non corrupted mul() instructions
-// - use an accumulator variable to keep running sum of mul()s as they're found
-//   ( use int of appropriate type )
-
-
-int main(){
-  std::string fname = "test_input.txt"
-
+uint64_t find_muls(auto line) {
+  auto begin = std::sregex_iterator(line.begin(), line.end(), multiply_pattern);
+  auto end = std::sregex_iterator();
+  uint64_t accumulator{0};
   // use capture groups, because reasons
   std::regex multiply_pattern(R"(mul\((\d+),(\d+)\))");
-  
+
+  for (auto it = begin; it != end; ++it) {
+    std::smatch match = *it;
+    int num1 = std::stoi(match[1].str());
+    int num2 = std::stoi(match[2].str());
+    accumulator += num1 * num2;
+  }
+  return accumulator;
+}
+
+void process_file(std::string filename) {
+  std::ifstream fs(filename);
+  std::string line{};
+  uint64_t accumulated_sum{0};
+
+  while (std::getline(fs, line)) {
+    accumulated_sum += find_muls(line);
+  }
+  std::cout << accumulated_sum << "\n";
+}
+
+int main() {
+  std::string fname = "input.txt";
+
+  process_file(fname);
+
 }
