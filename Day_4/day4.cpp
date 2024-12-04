f6e0f7ff81e596b0ad561679ab357baf804ba56f
Bill Blum <bill.blum@gmail.com>
2024-12-04 18:58:17 -0500

    On my way.

--- Day_4/day4.cpp
+++ Day_4/day4.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using wordsearch = std::vector<std::vector<char>>;
 
@@ -36,10 +37,24 @@ void print_puzzle(wordsearch puzzle){
   }
 }
 
+int count_occurrences(std::string line, std::string substring){
+  int count{0};
+  size_t pos_substr = line.find(substring, 0);
+  while (pos_substr != std::string::npos) {
+    count++;
+    pos_substr = line.find(substring, pos_substr + 1);
+  }
+  return count;
+}
+
 void part_one(wordsearch puzzle, std::string str){
   int num_found{0};
+  std::string rev_str = str;
+  std::reverse(rev_str.begin(), rev_str.end());
+
   int strlength = str.length();
-  
+  int height = puzzle.size();
+  int width = puzzle[0].size();
   // OK, this is how it's going down.
   // - First, search for the word reading L->R
   //   in each row starting at column 0, ending
@@ -50,12 +65,28 @@ void part_one(wordsearch puzzle, std::string str){
   //   the edges compared to length of the word
 
   // search the *rows*
-
+  for (auto row = 0; row<height; row++){
+    std::string row_text;
+    for( auto ch : puzzle[row]){
+      row_text+=ch;
+    }
+    num_found += count_occurrences(row_text, str);
+    num_found += count_occurrences(row_text, rev_str);
+  }
+  
   // search the *columns*
-
+  for (auto col = 0; col<width; col++){
+    std::string col_text;
+    for( auto row =0; row<puzzle.size(); row++){
+      col_text += puzzle[row][col];
+    }
+    num_found += count_occurrences(col_text, str);
+    num_found += count_occurrences(col_text, rev_str);
+  }
   // search the *diagonals*
 
 
+  std::cout << "Part 1: Number found: " << num_found << "\n";
 
 }
 
@@ -65,6 +96,10 @@ int main() {
 
   wordsearch puzzle = load_puzzle(fname);
   print_puzzle(puzzle);
-  // part_one(puzzle);
+  // int height = puzzle.size();
+  // int width = (height > 0) ? puzzle[0].size() : 0;
+  // std::cout << width << " wide x " << height << " high\n.";
+
+  part_one(puzzle, str);
   // part_two(puzzle);
 }
