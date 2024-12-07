6be9a92419b5d8aecc8c994059c41fc326c5434c
Bill Blum <bill.blum@gmail.com>
2024-12-07 09:41:33 -0500

    saving state

--- Day_7/day7.cpp
+++ Day_7/day7.cpp
@@ -4,14 +4,15 @@
 #include <unordered_map>
 #include <string>
 #include <sstream>
+#include <optional>
 
-struct unsolved_equation{
+struct calibration_equation{
   int result;
   std::vector<int> terms;
   std::vector<char> operations;
 };
 
-using calibration_list = std::vector<unsolved_equation>;
+using calibration_list = std::vector<calibration_equation>;
 
 calibration_list loader(const std::string& filename){
   std::ifstream fs(filename);
@@ -21,7 +22,7 @@ calibration_list loader(const std::string& filename){
   while(std::getline(fs, line)){
     std::istringstream iss(line);
     std::string substring{};
-    unsolved_equation eqn;
+    calibration_equation eqn;
     std::vector<int> terms;
 
     std::getline(iss, substring, ':');
@@ -30,16 +31,73 @@ calibration_list loader(const std::string& filename){
     
     while(std::getline(iss, substring, ',')){
       std::cout << substring << " ";
-      eqn.terms.push_back(std::stoi(substring));
+      int tmp = std::stoi(substring);
+      terms.push_back(tmp);
     }
+    std::cout << "\nterms size: " << terms.size() << std::endl;
+    // eqn.terms = std::copy(terms);
+
     std::cout << "\n";
     list.push_back(eqn);
   }
   return list;
 }
 
+bool check_equation(calibration_equation& eqn){
+  int result = eqn.terms[0];
+
+  for(size_t op = 0; op<eqn.operations.size(); op++){
+    if(eqn.operations[op]=='+'){
+      result += eqn.terms[op+1];
+    } else {
+      result *= eqn.terms[op+1];
+    }
+  }
+  return result == eqn.result;
+}
+
+
+void part_one(calibration_list& eqns){
+  calibration_list valid_eqns;
+
+  for(auto& eqn : eqns){
+    int num_of_ops = eqn.terms.size();
+    calibration_equation test_eqn;
+
+    test_eqn.result = eqn.result;
+    test_eqn.terms = eqn.terms;
+    // iterate over all values of unsigned int of appropriate size
+    // ( this will, by default, generate all permutations of 0 and 1 and we only have two operations)
+    // for(unsigned long int i = 0; i < (1<<num_of_ops) ; i++){
+    unsigned long int i = 0;
+    do{
+      for(auto bit=0; bit<num_of_ops; bit++){
+        test_eqn.operations.clear();
+        // 0 = add 1 = mul
+        char op = ( i & ( 1 << bit )) ? '*' : '+';
+        test_eqn.operations.push_back(op);
+      }
+
+      // std::cout << test_eqn.result << " " << test_eqn.terms.size() << " " << eqn.terms.size() <<"\n";
+      if(check_equation(test_eqn)){
+        valid_eqns.push_back(test_eqn);
+        break;
+      }
+
+      i++;
+    
+    } while (i < ( 1<<num_of_ops));
+  }
+  std::cout << "Pt 1: Num valid eqns: " << valid_eqns.size() << "\n";
+
+}
 int main(){
   std::string fname = "test_input.txt";
   calibration_list calibrations = loader(fname);
+  // for(auto eqn:calibrations){
+  //   std::cout << eqn.result <<"\n";
+  //   std::cout << eqn.terms.size() << "\n";
+  // }
+  // part_one(calibrations);
 
 }
