5dd8228b321be7cf7b992f8e9208447af09b1250
Bill Blum <bill.blum@gmail.com>
2024-12-17 18:31:18 -0500

    Saving state.

---
+++ Day_14/day14.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+struct Robot{
+  int x{};
+  int y{};
+  int xdot{};
+  int ydot{};
+};
+
+int main(){
+  std::string fname = "test_input.txt";
+
+}
